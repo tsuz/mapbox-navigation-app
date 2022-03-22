fabff79e1e0690ad1c2a36ee4b035dbf0c486524
Takuto Suzuki <takuto.suzuki@mapbox.com>
2022-03-22 16:04:17 +0900

    Initial Commit

---
+++ Shared/mapbox_navigation_jpApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapbox_navigation_jpApp.swift
+//  Shared
+//
+//  Created by Taku Suzuki on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct mapbox_navigation_jpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
